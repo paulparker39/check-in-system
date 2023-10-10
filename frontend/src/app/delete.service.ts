5e1869974fefce2392a01ab84ffede38f3af3b75
Vishal Erninti <vish_erninti@yahoo.com>
2023-10-10 13:49:18 -0400

    trying again

--- frontend/src/app/delete.service.ts
+++ frontend/src/app/delete.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError, } from 'rxjs';
 
 @Injectable({
+  
   providedIn: 'root'
 })
 export class DeleteService {
@@ -10,8 +11,8 @@ export class DeleteService {
   constructor(private http: HttpClient) { }
 
   deleteUser(pid: number): Observable<any> {
-    
     return this.http.delete(`/api/registrations/${pid}`);
+
   }
 
 }
